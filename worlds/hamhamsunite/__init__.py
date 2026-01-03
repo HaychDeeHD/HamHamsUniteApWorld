7a6cad1a386a660e35b9bf2ed5feb596c6182598
HaychDeeHd <haychdeehaychdee@gmail.com>
2026-01-03 03:44:31 -0800

    Basic generation working!

--- worlds/hamhamsunite/__init__.py
+++ worlds/hamhamsunite/__init__.py
@@ -1,38 +1,82 @@
 import typing
+
+from BaseClasses import Region
+from Utils import visualize_regions
+from worlds.generic.Rules import add_rule
 from .options import HamHamsUniteGameOptions
-from .items import ham_hams_unite_items
-from .locations import ham_hams_unite_locations
+from .items import HAMCHATS, HamHamsUniteItem
+from .locations import ACORN_SHRINE_LOCATION_DATAS, CLUBHOUSE_LOCATION_DATAS, LOCATION_DATAS, HamHamsUniteLocation
 from .settings import HamHamsUniteSettings
 from worlds.AutoWorld import World
 
 
 class MyGameWorld(World):
     """Insert description of the world/game here."""
-    game = "Hamtaro: Ham-Hams Unite!"
+    game = "Ham Hams Unite"
     options_dataclass = HamHamsUniteGameOptions
     options: HamHamsUniteGameOptions
     settings: typing.ClassVar[HamHamsUniteSettings]  # will be automatically assigned from type hint
     topology_present = True  # show path to required location checks in spoiler
 
-    # This base_id bizz is pretty unnecessary. Each item/loc def needs a unique id.
-    # That can just be part of the item. And there's no reason it can't start at 1.
-    # There can be an Item 5 and a Location 5, that's not a conflict.
+    item_name_to_id = {itemdata.name: itemdata.id for itemdata in HAMCHATS}
+    location_name_to_id = {locationdata.name: locationdata.id for locationdata in LOCATION_DATAS}
 
-    # ID of first item and location, could be hard-coded but code may be easier
-    # to read with this as a property.
-    base_id = 1234
-    # instead of dynamic numbering, IDs could be part of data
 
-    # The following two dicts are required for the generation to know which
-    # items exist. They could be generated from json or something else. They can
-    # include events, but don't have to since events will be placed manually.
-    item_name_to_id = {name: id for
-                       id, name in enumerate(ham_hams_unite_items, base_id)}
-    location_name_to_id = {name: id for
-                           id, name in enumerate(ham_hams_unite_locations, base_id)}
+    def create_regions(self) -> None:
+        menu_region = Region("Menu", self.player, self.multiworld)
+        self.multiworld.regions.append(menu_region)
 
-    # Items can be grouped using their names to allow easy checking if any item
-    # from that group has been collected. Group names can also be used for !hint
-    # item_name_groups = {
-    #     "weapons": {"sword", "lance"},
-    # }
+        # TODO Generalize this region creation
+
+        clubhouse_region = Region("Clubhouse", self.player, self.multiworld)
+        clubhouse_region.locations = [HamHamsUniteLocation(location_data, self.player, clubhouse_region) for location_data in CLUBHOUSE_LOCATION_DATAS]
+        self.multiworld.regions.append(clubhouse_region)
+
+
+        acorn_shrine_region = Region("Acorn Shrine", self.player, self.multiworld)
+        acorn_shrine_region.locations = [HamHamsUniteLocation(location_data, self.player, acorn_shrine_region) for location_data in ACORN_SHRINE_LOCATION_DATAS]
+        self.multiworld.regions.append(acorn_shrine_region)
+
+
+        menu_region.connect(clubhouse_region)
+        # TODO may not be 2 way because dig-dig
+        clubhouse_region.connect(acorn_shrine_region)
+
+
+    def create_item(self, name: str) -> HamHamsUniteItem:
+        # TODO remove traversal with a map
+        for dataitem in HAMCHATS:
+            if dataitem.name == name:
+                return HamHamsUniteItem(dataitem, self.player)
+
+
+    def create_items(self) -> None:
+        for dataitem in HAMCHATS:
+            self.multiworld.itempool.append(self.create_item(dataitem.name))
+
+
+    def set_rules(self) -> None:
+
+        def has(name):
+            return lambda state: state.has(name, self.player)
+
+        # This won't be in the final APWorld except maybe as an alternate win con.
+        def has_n_chats(n):
+            return lambda state: state.has_from_list_unique([hamchat.name for hamchat in HAMCHATS], self.player, n)
+
+
+        add_rule(self.multiworld.get_location("Dreaming Hamster (Mega-Q)", self.player), has("Hamha"))
+        add_rule(self.multiworld.get_location("Flower Hamster (Koochi-Q)", self.player), has("Hif-hif"))
+        add_rule(self.multiworld.get_location("Rooftop Hamster (Nopibloo)", self.player), has("Tack-Q"))
+        add_rule(self.multiworld.get_location("Rooftop Hamster (Nopibloo)", self.player), has("Scoochie"))
+        # TODO replace the nopibloo requirements with an event (rope) to test that out?
+        add_rule(self.multiworld.get_location("Lovesick Hamster (Teenie)", self.player), has("Tack-Q"))
+        add_rule(self.multiworld.get_location("Maxwell (Bizarroo)", self.player), has("Tack-Q"))
+        add_rule(self.multiworld.get_location("Maxwell (Bizarroo)", self.player), has("Nopibloo"))
+
+
+        self.multiworld.completion_condition[self.player] = has_n_chats(16)
+
+        # Uncomment to visualize world for debugging
+        visualize_regions(self.multiworld.get_region("Menu", self.player), "my_world.puml")
+
--- worlds/hamhamsunite/locations.py
+++ worlds/hamhamsunite/locations.py
@@ -1,7 +1,58 @@
 from BaseClasses import Location
 
-
+# This class represents a location in the multiworld. It will be constructed at generation time.
+# This location class only makes sense in a context where there is a defined player and multiworld.
 class HamHamsUniteLocation(Location):
-    game = "Hamtaro: Ham-Hams Unite!"
+    game = "Ham Hams Unite"
 
-ham_hams_unite_locations = []
+    def __init__(self, location_data, player, region):
+        super().__init__(player, location_data.name, location_data.id, region)
+        self.location_data = location_data
+
+
+# This is an internal class for defining static item data.
+# It is independent of any player or multiworld.
+# Instances will be constructed statically in this file. 
+# It will only be used by this AP World's own code.
+class HamHamsUniteLocationData():
+    name: str
+    id: str
+    # classification: ItemClassification
+    # type: str # TODO enum?
+
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id 
+        # self.classification = classification
+        # self.type = type
+
+# TODO in the future, subclasses may be helpful
+
+
+def create_location_datas(location_name_list, base_id):
+    return [HamHamsUniteLocationData(name, base_id + id) for id, name in enumerate(location_name_list)]
+
+CLUBHOUSE_LOCATION_DATAS = create_location_datas([
+    "Boss Starting HamChat 1",
+    "Boss Starting HamChat 2",
+    "Boss Starting HamChat 3",
+    "Boss Starting HamChat 4",
+    "Boss Starting HamChat 5",
+    "Boss Starting HamChat 6",
+    "Boss Starting HamChat 7",
+    "Boss Starting HamChat 8",
+    "Boss Starting HamChat 9",
+    "Boss Starting HamChat 10",
+    "Boss Starting HamChat 11",
+], 1000)
+
+ACORN_SHRINE_LOCATION_DATAS = create_location_datas([
+    "Dreaming Hamster (Mega-Q)",
+    "Flower Hamster (Koochi-Q)",
+    "Rooftop Hamster (Nopibloo)",
+    "Lovesick Hamster (Teenie)",
+    "Maxwell (Bizarroo)",
+    # Maxwell, Friend
+], 2000)
+
+LOCATION_DATAS = CLUBHOUSE_LOCATION_DATAS + ACORN_SHRINE_LOCATION_DATAS
